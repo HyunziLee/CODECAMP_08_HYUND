401307c6f0287b77ce8f6c879caeb7c48962dcb6
HyunziLee <hyunzi940@naver.com>
2022-07-31 15:06:53 +0900

    메인페이지

--- freeboard_frontend/pages/index.js
+++ freeboard_frontend/pages/index.js
@@ -1,14 +1,123 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import * as s from "../styles/home.styles";
+import { useMediaQuery } from "react-responsive";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useRouter } from "next/router";
+import { SwapRightOutlined } from "@ant-design/icons";
 
 export default function Home() {
   const BgVideo = "/video/bgWave.mp4";
+
+  const isPc = useMediaQuery({
+    query: "(min-width:1024px)",
+  });
+  const isTablet = useMediaQuery({
+    query: "(min-width:768px) and (max-width:1023px)",
+  });
+  const isMobile = useMediaQuery({
+    query: "(max-width:767px)",
+  });
+
+  const [isHover, setIsHover] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    AOS.init();
+  });
+
+  const onHover = () => {
+    setIsHover(!isHover);
+  };
+
+  const onClickGoHome = () => {
+    router.push("/PostList/p");
+  };
+
   return (
     <>
-      <s.Wrapper>
-        <s.Video autoPlay muted loop src={BgVideo}></s.Video>
-        <s.Video_text>hello</s.Video_text>
-      </s.Wrapper>
+      {isPc && (
+        <s.Wrapper>
+          <s.Video autoPlay muted loop src={BgVideo} width={`2560px`}></s.Video>
+          {isHover ? (
+            <s.Video_text
+              fontsize={`32px`}
+              onMouseOver={onHover}
+              width={`150px`}
+              height={`150px`}
+            >
+              Hello
+            </s.Video_text>
+          ) : (
+            <s.GoHome
+              onMouseLeave={onHover}
+              fontsize={`25px`}
+              onClick={onClickGoHome}
+              width={`150px`}
+              height={`150px`}
+            >
+              Go Home
+              <SwapRightOutlined />
+            </s.GoHome>
+          )}
+        </s.Wrapper>
+      )}
+      {isTablet && (
+        <s.Wrapper className="container">
+          <s.Video autoPlay muted loop src={BgVideo} width={`2560px`}></s.Video>
+
+          {isHover ? (
+            <s.Video_text
+              fontsize={`32px`}
+              onMouseOver={onHover}
+              width={`150px`}
+              height={`150px`}
+            >
+              Hello
+            </s.Video_text>
+          ) : (
+            <s.GoHome
+              onMouseLeave={onHover}
+              fontsize={`25px`}
+              onClick={onClickGoHome}
+              width={`150px`}
+              height={`150px`}
+            >
+              Go Home
+              <SwapRightOutlined />
+            </s.GoHome>
+          )}
+        </s.Wrapper>
+      )}
+      {isMobile && (
+        <s.Wrapper>
+          <s.Video autoPlay muted loop src={BgVideo} width={`390px`}></s.Video>
+          {isHover ? (
+            <s.Video_text
+              fontsize={`10px`}
+              onMouseOver={onHover}
+              width={`60px`}
+              height={`60px`}
+            >
+              Hello
+            </s.Video_text>
+          ) : (
+            <s.GoHome
+              onMouseLeave={onHover}
+              fontsize={`8px`}
+              onClick={onClickGoHome}
+              width={`60px`}
+              height={`60px`}
+            >
+              Go Home
+              <SwapRightOutlined />
+            </s.GoHome>
+          )}
+        </s.Wrapper>
+      )}
+      <s.Main_intro>
+        <s.Intro_contents data-aos="fade-up" />
+      </s.Main_intro>
     </>
   );
 }
--- freeboard_frontend/styles/home.styles.ts
+++ freeboard_frontend/styles/home.styles.ts
@@ -1,16 +1,57 @@
 import styled from "@emotion/styled";
+import { propTypes } from "react-bootstrap/esm/Image";
 
 export const Wrapper = styled.div`
   position: relative;
-  width: 1000px;
+  width: ${(props) => props.width};
 `;
 
 export const Video = styled.video`
-  /* height: 1000px; */
+  width: 100%;
+  overflow: scroll;
 `;
 
 export const Video_text = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  line-height: ${(props) => props.height};
+  border-radius: 50%;
+  text-align: center;
+
+  font-size: ${(props) => props.fontsize};
+  cursor: pointer;
+`;
+
+export const GoHome = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  line-height: ${(props) => props.height};
+  text-align: center;
+
+  background-color: #bbd0ff;
+  color: #fff;
+  font-weight: 800;
+  border-radius: 50%;
+  font-size: ${(props) => props.fontsize};
+  cursor: pointer;
+`;
+
+export const Main_intro = styled.div`
+  width: 100%;
+  height: 800px;
+  background-color: yellow;
+  position: absolute;
+`;
+
+export const Intro_contents = styled.div`
+  width: 200px;
+  height: 100px;
+
+  background-color: skyblue;
 `;
