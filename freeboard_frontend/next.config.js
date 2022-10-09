f88d2fc21b0b6a684bd66af99deaccbfb3abe10e
HYUN D <hyunzi940@naver.com>
2022-10-09 15:33:34 +0900

    update

--- freeboard_frontend/next.config.js
+++ freeboard_frontend/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
-  assetPrefix: isProd ? 'https://pick-pick.shop' : undefined,
+  
 
 }
 
